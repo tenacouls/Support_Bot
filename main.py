0f1d7b70b19136f359dc4ba228daa7b20737d314
tenacouls <playindasun@gmail.com>
2023-07-18 00:26:12 +0300

    База данных не работает, не выводятся сообщения об обнаружении имени организации соответственно ИНН

--- main.py
+++ main.py
@@ -1,34 +1,57 @@
+import sqlite3
 from typing import Final
-from telegram import Update
+from telegram import Update, Bot
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
 
 TOKEN = '6332766112:AAEjq_fSBrHXA5WdMKFm_3zpIrVxQ4mvxw4'
 BOT_USERNAME: Final = "@DocrobotSupport_bot"
 
+db_path = '/home/makeouthill/BOT_DB/Clients_DataBase'
+conn = sqlite3.connect(db_path)
+cursor = conn.cursor()
+
 #Описание команд для (start,help custom)
-async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def start_command(update: Update, context: Bot):
     
     await update.message.reply_text('Добрый день! Спасибо, что обратились за поддержкой именно ко мне, буду рад помочь вам, прошу, введите ваш ИНН чтобы я мог знать какой организации требуется моя помощь')
 
-async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def help_command(update: Update, context: Bot):
     
     await update.message.reply_text('Я могу выполнять следующие функции!')
     
 async def custom_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     
     await update.message.reply_text('Здесь будет доп описание')
+
     
 # Ответы от бота на сообщения пользователя
 
-def handle_response(text:str) -> str:
-    processed: str = text.lower()
-    if 'Привет' in processed:
-        return 'Добро пожаловать в отдел технической поддержки!'
+def handle_response(update: Update, context: Bot) -> str:
     
-    return 'Пока я не понимаю данные запросы('
+    try:
+        inn: int = update.message.chat.type
+        text
+    except ValueError:
+        update.message.reply_text('Введите корректное число.')
+        return
+    try:
+        cursor.execute('SELECT Comp_Name FROM Companys WHERE id=?', (inn,))
+        row = cursor.fetchone()
+
+        if row is not None:
+            update.message.reply_text('Организация ' + row[0] + ' успешно обнаружена')
+        else:
+            update.message.reply_text('Организация не найдена')
+
+    except sqlite3.Error as e:
+        update.message.reply_text('Произошла ошибка при обработке вашего запроса.')
+        print('Ошибка:', e)
+    
+    updater = Update(TOKEN, use_context=True)
+
 
 # Ответ от бота при если он числится в груповом чате.
-async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def handle_message(update: Update, context: Bot):
     message_type: str = update.message.chat.type
     text: str = update.message.text
     
@@ -46,8 +69,8 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(response)
     
     
-async def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    print(f'Update{update} causef error {context.error}')
+async def error(update: Update, context: Bot):
+    print(f'{update} cause of error {context.error}')
     
     
 if __name__ == '__main__':
