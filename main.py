64bb23ae263388be7e8732cbbc2e3c03d9407cce
Гребнев Анатолий <playindasun@gmail.ru>
2023-07-17 17:52:56 +0300

    Add some feauters to bot

---
+++ main.py
@@ -0,0 +1,72 @@
+from typing import Final
+from telegram import Update
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
+
+TOKEN = '6332766112:AAEjq_fSBrHXA5WdMKFm_3zpIrVxQ4mvxw4'
+BOT_USERNAME: Final = "@DocrobotSupport_bot"
+
+#Описание команд для (start,help custom)
+async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    
+    await update.message.reply_text('Добрый день! Спасибо, что обратились за поддержкой именно ко мне, буду рад помочь вам, прошу, введите ваш ИНН чтобы я мог знать какой организации требуется моя помощь')
+
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    
+    await update.message.reply_text('Я могу выполнять следующие функции!')
+    
+async def custom_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    
+    await update.message.reply_text('Здесь будет доп описание')
+    
+# Ответы от бота на сообщения пользователя
+
+def handle_response(text:str) -> str:
+    processed: str = text.lower()
+    if 'Привет' in processed:
+        return 'Добро пожаловать в отдел технической поддержки!'
+    
+    return 'Пока я не понимаю данные запросы('
+
+# Ответ от бота при если он числится в груповом чате.
+async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    message_type: str = update.message.chat.type
+    text: str = update.message.text
+    
+    print(f'User ({update.message.chat.id}) in {message_type}: "{text}"')
+    
+    if message_type == 'group':
+        if BOT_USERNAME in text:
+            new_text: str = text.replace(BOT_USERNAME, '').strip()
+            response :str = handle_response(new_text)
+        else:
+            return
+    else:
+        response: str = handle_message(text)
+    print('Bot', response)
+    await update.message.reply_text(response)
+    
+    
+async def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    print(f'Update{update} causef error {context.error}')
+    
+    
+if __name__ == '__main__':
+    print('Бот Запускается...')
+    app = Application.builder().token(TOKEN).build()
+    
+    # Команды
+    app.add_handler(CommandHandler('start', start_command))
+    app.add_handler(CommandHandler('help', help_command))
+    app.add_handler(CommandHandler('custom', custom_command))
+    
+    #Сообщения
+    
+    app.add_handler(MessageHandler(filters.TEXT, handle_message))
+    
+    #Ошибки
+    app.add_error_handler(error)
+    
+    
+    #Проверка новых сообщений
+    print ('Проверяю сообщения')
+    app.run_polling(poll_interval=3)
